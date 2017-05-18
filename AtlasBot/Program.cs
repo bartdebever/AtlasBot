92a4efa0613e45d51b959f7c16c3c8ed0bc27893
Bever,Bart B. de <357013@student.fontys.nl>
2017-05-18 19:33:02 +0200

    Bot weirdly not working, trying to commit to fix it

--- AtlasBot/Program.cs
+++ AtlasBot/Program.cs
@@ -130,7 +130,7 @@ namespace AtlasBot
                 commands.CreateCommand("legal")
                     .Do(async (e) =>
                     {
-                        await e.Channel.SendMessage("AtlasBot isn’t endorsed by Riot Games and doesn’t reflect the views or opinions of Riot Games or anyone officially involved in producing or managing League of Legends. League of Legends and Riot Games are trademarks or registered trademarks of Riot Games, Inc. League of Legends © Riot Games, Inc.");
+                        await e.Channel.SendMessage("AtlasBot isn’t endorsed by Riot Games and doesn’t reflect the views or opinions of Riot Games or anyone officially involved in producing or managing League of Legends. League of Legends and Riot Games are trademarks or registered trademarks of Riot Games, Inc. League of Legends © Riot Games, Inc");
                     });
             }
 
